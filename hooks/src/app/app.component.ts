55b02d7cf68f84f8f269b75c42e32338a73e8318
Daniela <danielapb78@gmail.com>
2024-02-05 21:24:32 -0500

    lab hooks

---
+++ hooks/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hooks';
+  counter= 0;
+
+  constructor() {
+      console.log('constructor');     
+      this.counter = 1;
+  }
+
+  inc(){
+      console.log('inc');
+      this.counter += 1;
+  }
+
+  ngOnInit(){
+      console.log('ng on init');
+  }
+
+  ngOnDestroy(){
+      console.log('ngOnDestroy');
+  }
+
+  ngDoCheck(){
+      console.log('ngDoCheck');
+  }
+
+  ngOnChanges(){
+      console.log('ngOnChanges');
+  }
+
+  ngAfterContentInit(){
+      console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+      console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+      console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(){
+      console.log('ngAfterViewChecked');
+  }
+}
+
